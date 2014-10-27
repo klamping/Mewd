3ea0e4f9c09c9cb53831e83edca482a624fc76dd
Kevin Lamping <klamping@gmail.com>
2014-10-26 22:00:40 -0500

    fix folder copy issue

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -6,7 +6,7 @@ mkdir out;
  git init
  git config user.name "Travis-CI"
  git config user.email "travis@mewdapp.com"
- cp -r ../www ./
+ cp -r ../www/* .
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
