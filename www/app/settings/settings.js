5e2fb2cd8ab66a6f7696edf6b1de3bb0e81b7457
Kevin Lamping <kevin.lamping@rackspace.com>
2014-11-01 00:34:05 -0500

    added password change functionality

--- www/app/settings/settings.js
+++ www/app/settings/settings.js
@@ -1,15 +1,43 @@
 angular.module('moodTracker')
-.controller('SettingsCtrl', function($scope, $rootScope, $ionicPopup) {
+.controller('SettingsCtrl', function($scope, $rootScope, $ionicPopup, $ionicPopover) {
     // Logs a user out
     $scope.logout = function() {
         $rootScope.auth.$logout();
     };
 
+    $ionicPopover.fromTemplateUrl('changePassword.html', {
+        scope: $scope,
+    }).then(function (popover) {
+        $scope.popover = popover;
+    });
+
+    $scope.openPopover = function ($event) {
+        $scope.popover.show($event);
+    };
+
+    $scope.changePassword = function (oldPw, newPw) {
+        $rootScope.auth.$changePassword($rootScope.user.email, oldPw, newPw)
+        .then(function () {
+            $scope.popover.hide();
+            $ionicPopup.alert({
+                title: 'Password Changed'
+            });
+        }, function (err) {
+            $ionicPopup.alert({
+                title: 'Error Changing Password',
+                template: err
+            });
+        });
+    };
+
     $scope.deleteAccount = function() {
+        $scope.errMsg = '';
+
         $ionicPopup.prompt({
             title: 'Delete Your Account',
             subTitle: 'Type your account password',
             inputType: 'password',
+            inputPlaceholder: 'Enter Password',
             okText: 'Delete',
             okType: 'button-assertive'
         }).then(function (res) {
@@ -24,7 +52,7 @@ angular.module('moodTracker')
                     template: 'Thanks for trying Mewd out!'
                 });
             }, function (err) {
-                $scope.deleteError = err.message;
+                $scope.errMsg = err.message;
             });
         });
     };
