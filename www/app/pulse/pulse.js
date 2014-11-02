02b017f02321af3457a17db068cf012185222bf9
Kevin Lamping <kevin.lamping@rackspace.com>
2014-11-01 22:25:08 -0500

    added mood history graphs

--- www/app/pulse/pulse.js
+++ www/app/pulse/pulse.js
@@ -2,61 +2,91 @@ angular.module('moodTracker')
 .controller('MoodPulseCtrl', function ($scope, moods, moodRecord) {
     var storedMoods = moods.$asArray();
     var records = moodRecord.$asArray();
-    $scope.dayByDay = [0];
-    var currentDay = 0;
 
     var getCounts = function (records) {
         var counts = [0, 0, 0];
 
         _.each(records, function (record) {
-            counts[record.positive + 1]++;
+            counts[record.positivity + 1]++;
         });
 
         return counts;
     };
 
+    $scope.runningPositivity = [];
+    $scope.runningLabels = [];
 
-
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
-
-
-
-    $scope.polarColors = [
+    $scope.distributionColors = [
         Chart.defaults.global.colours[2],
         Chart.defaults.global.colours[1],
         Chart.defaults.global.colours[3]
     ];
 
+    $scope.distributionLabels = ['negative', 'neutral', 'positive'];
 
-    storedMoods.$loaded().then(function () {
-        records.$loaded().then(function () {
+    records.$loaded().then(function () {
+        storedMoods.$loaded().then(function () {
+
+            // if saved w/o a positivity value, update the mood
+            // TODO this can be removed after all data has been updated
             _.each(records, function (record) {
                 // find the associate mood
                 var mood = _.find(storedMoods, { 'name': record.mood });
+                if (_.isObject(mood) && !('positivity' in record)) {
+                    record.positivity = mood.positive;;
 
-                if (_.isObject(mood)) {
-                    record.positive = mood.positive;
-
-                    var dayPositivity = $scope.dayByDay[currentDay] + record.positive;
-                    $scope.dayByDay.push(dayPositivity)
-                    currentDay++;
+                    records.$save(record);
                 }
             });
-
-            $scope.counts = getCounts(records);
-
-            var today = new Date();
-            var sevenDaysAgo = today.setDate(today.getDate() - 7);
-            var lastSevenDays = _.filter(records, function (record) {
-                return record.time > sevenDaysAgo;
-            });
-
-            $scope.countSeven = getCounts(lastSevenDays);
         });
+
+        var accumulator = 0;
+        var currentMonth = 0;
+        var currentYear = 0;
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        var label = '';
+        $scope.runningPositivity = _.map(records, function (record) {
+            if (_.isObject(record) && 'positivity' in record) {
+                // convert record time to a day
+                var time = new Date(record.time);
+                var recordMonth = time.getMonth();
+                var recordYear = time.getFullYear();
+
+                if (recordMonth > currentMonth || recordYear > currentYear) {
+                    label = months[recordMonth];
+                    if (recordYear > currentYear) {
+                        label += ' ' + recordYear;
+                        currentYear = recordYear;
+                    }
+                    $scope.runningLabels.push(label);
+                    currentMonth = recordMonth;
+                } else {
+                    $scope.runningLabels.push('');
+                }
+
+                accumulator += record.positivity;
+
+                return accumulator;
+            }
+        });
+
+        // add to an array so that chart.js is happy
+        $scope.runningPositivity = [$scope.runningPositivity];
+        $scope.runningOptions = {
+            pointDot: false,
+            scaleShowGridLines: false,
+            showTooltips: false,
+            responsive: true
+        };
+
+        $scope.counts = getCounts(records);
+
+        var today = new Date();
+        var sevenDaysAgo = today.setDate(today.getDate() - 7);
+        var lastSevenDays = _.filter(records, function (record) {
+            return record.time > sevenDaysAgo;
+        });
+
+        $scope.countSeven = getCounts(lastSevenDays);
     });
 });
--- www/app/record/record.js
+++ www/app/record/record.js
@@ -1,26 +1,31 @@
 angular.module('moodTracker')
 .controller('RecordCtrl', function($scope, $firebase, moods, moodRecord, $ionicPopup) {
-  $scope.setMood = function (mood) {
-    var name = mood.name;
-    $ionicPopup.confirm({
-      title: 'Feeling ' + name + '?',
-      okText: 'Yep',
-      cancelText: 'Nope'
-    }).then(function (hasConfirmed) {
-      if (hasConfirmed) {
+    var saveMood = function (mood) {
         moodRecord.$asArray().$add({
-          mood: name,
-          time: Date.now()
+            mood: mood.name,
+            positivity: mood.positive,
+            time: Date.now()
         });
-      }
-    });
-  };
+    };
 
-  $scope.moods = moods.$asArray();
+    $scope.setMood = function (mood) {
+        var name = mood.name;
+        $ionicPopup.confirm({
+            title: 'Feeling ' + name + '?',
+            okText: 'Yep',
+            cancelText: 'Nope'
+        }).then(function (hasConfirmed) {
+            if (hasConfirmed) {
+                saveMood(mood);
+            }
+        });
+    };
 
-  $scope.moodFilter = {};
+    $scope.moods = moods.$asArray();
 
-  $scope.filterMood = function (positivity) {
-    $scope.moodFilter.positive = positivity;
-  };
+    $scope.moodFilter = {};
+
+    $scope.filterMood = function (positivity) {
+        $scope.moodFilter.positive = positivity;
+    };
 });
