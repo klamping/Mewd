3391b50563418db0c68232c2fc44dcb91b177fd5
Kevin Lamping <klamping@gmail.com>
2014-11-17 23:05:12 -0600

    removed local dependencies and replaced with bower

--- www/app/app.js
+++ www/app/app.js
@@ -53,7 +53,7 @@ angular.module('moodTracker', ['ionic', 'firebase', 'ui.router', 'chart.js', 'ng
     $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
-        if(window.cordova && window.cordova.plugins.Keyboard) {
+        if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
         if(window.StatusBar) {
